58913e4611728783262dc739cbd55cfb296bda87
Paul Garner <paul@potatolondon.com>
2011-08-12 17:30:32 +0100

    make PIP installable (?)

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-tempocal',
+    version='0.1',
+    author=u'Anentropic',
+    packages=find_packages(),
+    url='https://github.com/anentropic/django-tempocal',
+    license='BSD',
+    description='A django-fied, templated version of Python\'s built-in HTMLCalendar.',
+    zip_safe=False,
+    include_package_data=True,
+)
